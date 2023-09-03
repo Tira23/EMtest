9a221aaa6948bb0f05105ec44d87d7e6f68c8f29
Tira <Tirael_god@mail.ru>
2023-09-03 20:56:16 +0300

    add unsubsribe

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,21 +1,24 @@
-import {Component, HostListener, OnInit} from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
 import {AppService} from "../../app.service";
 import {Post} from "../../interface/posts";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.scss']
 })
-export class PostComponent implements OnInit{
+
+export class PostComponent implements OnInit, OnDestroy{
   constructor(private route: ActivatedRoute, private appService: AppService, private router:Router) {
   }
   post:Post = {} as Post
   loading = true
+  sub!: Subscription
   ngOnInit() {
 
-    this.route.params.subscribe(params => {
+    this.sub = this.route.params.subscribe(params => {
       const id = params['id'];
       this.appService.getDetails(id).subscribe(data => {
         this.post = data
@@ -26,5 +29,9 @@ export class PostComponent implements OnInit{
   @HostListener('window:keydown.escape')
   exit(){
     this.router.navigate([`post`]).then()
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
   }
 }
--- src/app/components/posts/posts.component.ts
+++ src/app/components/posts/posts.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {AppService} from "../../app.service";
 import {Post} from "../../interface/posts";
 import {Router} from "@angular/router";
+import {Subscriber, Subscription} from "rxjs";
 
 
 @Component({
@@ -9,21 +10,28 @@ import {Router} from "@angular/router";
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.scss']
 })
-export class PostsComponent implements OnInit{
+export class PostsComponent implements OnInit, OnDestroy{
 
   constructor(private appService: AppService, private router: Router) {}
+
+
   color = '#3f51b57d';
   posts:Post[] = []
   loading = true
-
+  sub!: Subscription
 
   click(id:number){
     this.router.navigate([`post/${id}`]).then()
   }
   ngOnInit() {
-    this.appService.getAll().subscribe(data => {
+   this.sub = this.appService.getAll().subscribe(data => {
       this.posts = data
       this.loading = false
     })
   }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
+
+  }
+
 }
